e73ed2867ebef85e3117f1bf9be1afc6b62ab77c
bddoylan <bddoylan@gmail.com>
2022-01-04 19:25:47 -0500

    Setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import landingPage from './components/pages/landingPage';
+import {Route} from "react-router-dom";
+
+function App() {
+  return (
+      //<Route path="/" exact component={landingPage} />
+      <h1> Landing Page </h1>
+  );
+}
+
+export default App;
